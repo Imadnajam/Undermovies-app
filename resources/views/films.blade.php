cf57d98866033fa9c76921bffcf27f76c185f15c
imadnajam <imad00naj11@gmail.com>
2023-12-07 17:52:08 +0100

    add page to playe films

--- resources/views/films.blade.php
+++ resources/views/films.blade.php
@@ -71,7 +71,7 @@
             </ul>
 
             @php
-                $shuffledTops = $films;
+                $shuffledTops = $films['results'];
                 shuffle($shuffledTops);
 
             @endphp
@@ -82,31 +82,31 @@
                 @foreach ($shuffledTops as $film)
                     <li>
                         <div class="movie-card">
-                            <a href="{{ route('detailmovie', ['id' => $film['imdbID']]) }}">
+                            <a href="{{route('detailmovieI',['id' => $film['id']])}}">
                                 <figure class="card-banner">
-                                    <img src="{{ $film['Poster'] }}" alt="{{ $film['Title'] }}">
+                                    <img src="{{ 'https://image.tmdb.org/t/p/w300_and_h450_bestv2/' . $film['poster_path'] }}" alt="{{ $film['original_title'] }}">
                                 </figure>
                             </a>
 
                             <div class="title-wrapper">
-                                <a href="{{ route('detailmovie', ['id' => $film['imdbID']]) }}">
-                                    <h3 class="card-title">{{ $film['Title'] }}</h3>
+                                <a href="{{ route('detailmovieI', ['id' => $film['id']]) }}">
+                                    <h3 class="card-title">{{ $film['original_title'] }}</h3>
                                 </a>
 
-                                <time datetime="{{ $film['Released'] }}">{{ $film['Released'] }}</time>
+                                <time datetime="{{ $film['release_date'] }}">{{ $film['release_date'] }}</time>
                             </div>
 
                             <div class="card-meta">
-                                <div class="badge badge-outline">{{ $film['Rated'] }}</div>
+                                <div class="badge badge-outline">{{ $film['vote_average'] }}</div>
 
                                 <div class="duration">
                                     <ion-icon name="time-outline"></ion-icon>
-                                    <time datetime="{{ $film['Runtime'] }}">{{ $film['Runtime'] }}</time>
+                                    <time datetime=""></time>
                                 </div>
 
                                 <div class="rating">
                                     <ion-icon name="star"></ion-icon>
-                                    <data>{{ $film['imdbRating'] }}</data>
+                                    <data>{{ $film['vote_count'] }}</data>
                                 </div>
                             </div>
                         </div>
