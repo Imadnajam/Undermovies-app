f7cca9596a396f8658707ebc96521a39957aa697
imadnajam <imad00naj11@gmail.com>
2023-12-08 23:23:58 +0100

    Update the pricing page

--- resources/views/pricing-template.blade.php
+++ resources/views/pricing-template.blade.php
@@ -1,224 +1,88 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <meta http-equiv="x-ua-compatible" content="ie=edge" />
-    <title>UnderMovie | pricing</title>
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css" />
-    <!-- Google Fonts Roboto -->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" />
- 
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
-
-    <style>
-    
-      #intro {
-        /* Margin to fix overlapping fixed navbar */
-        margin-top: 58px;
-      }
-
-      @media (max-width: 991px) {
-        #intro {
-          /* Margin to fix overlapping fixed navbar */
-          margin-top: 45px;
-        }
-      }
-      body {
-        min-height: 100rem;
-      background-color:hsl(207, 19%, 11%);
-      
-    }
-    </style>
-
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Pricing Table</title>
+	<link href="https://fonts.googleapis.com/css?family=Maven+Pro:400,500,700&display=swap" rel="stylesheet">
+	<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+  <link rel="stylesheet" href="{{ asset('css/pricing1.css') }}">
 </head>
-<body >
-  <main class="mt-5">
-    <div class="container ">
-      <!--Section: Content-->
-      <section class="text-center">
-        <h4 class="mb-4" style="color: white;"><strong>Pricing</strong></h4>
+<body>
+
+
+<section class="table">
+	<div id="plan" class="table-header">
+		<span>misocial</span>
+		<h2>Select a Plan</h2>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,<br> sed do eiusmod
+		tempor incididunt ut</p>
+	</div>
+
+<div class="price-tags">
+	<div class="price-tag">
+	<img src="{{ asset('image/pricing/Asset3@4x.svg') }}" width="108" height="94">
+	 <span>personal</span>
+		<h4><sup>$</sup>0<sub>/month</sub></h4>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua.</p>
+		<a href="#" class="price-btn">Select plan</a>
+	</div>
+     
+     <div class="price-tag">
+		<img src="{{ asset('image/pricing/Asset2@4x.svg') }}" width="120" height="108">
+		<span>personal</span>
+		<h4><sup>$</sup>29<sub>/month</sub></h4>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua.</p>
+		<a href="#" class="price-btn">Select plan</a>
+	</div>
+     <div class="price-tag">
+		<img src="{{ asset('image/pricing/Asset1@4x.svg') }}" width="112" height="110">
+    
+		<span>personal</span>
+		<h4><sup>$</sup>49<sub>/month</sub></h4>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua.</p>
+		<a href="#" class="price-btn">Select plan</a>
+	</div>
+</div>
+</section>
+
+<div class="cta">
+	<span>social marketing & analytics</span>
+	<h4>Take control of your data</h4>
+	<a href="#" class="cta-btn">Get started</a>
+	<a href="#" class="cta-btn">Explore features</a>
+</div>
+
+<section class="footer-wrap">
+	<div class="footer">
+		<div class="logo">
+				<img src="{{ asset('image/series/logo.png') }}" width="100px">
         
- 
+	        </div>
+
+<div class="line"></div>
+<div class="legal">
+<div class="copyright">
+	<span>© 2023  Undermovies & NJ. Make the right impression.</span>
+</div>
+<div class="social-btns">
+	<a href="#"><i class="fa fa-facebook"></i></a>
+	<a href="#"><i class="fa fa-twitter"></i></a>
+	<a href="#"><i class="fa fa-google-plus"></i></a>
+	<a href="#"><i class="fa fa-instagram"></i></a>
+</div>
+</div>
+
+</section>
 
 
-        <div class="btn-group mb-4" role="group" aria-label="Basic example">
-          <button type="button" class="btn btn-primary active">Monthly billing</button>
-          <button type="button" class="btn btn-primary">
-            Annual billign <small>(2 months FREE)</small>
-          </button>
-        </div>
 
-        <div class="row">
 
-          <!--Grid column-->
-          <div class="col-lg-3 col-md-6 mb-4">
 
-            <!-- Card -->
-            <div class="card">
 
-              <div class="card-header bg-white py-3">
-                <p class="text-uppercase small mb-2"><strong>Free</strong></p>
-                <h5 class="mb-0">Free</h5>
-              </div>
 
-              <div class="card-body">
-                <ul  class="list-group list-group-flush">
-                  <li class="list-group-item">Standard library access.</li>
-                  <li class="list-group-item">Standard Definition (SD) streaming.</li>
-                  <li class="list-group-item">Ad-supported experience.</li>
-                  <li class="list-group-item">No offline download feature.</li>
-                  <li class="list-group-item">Limited to one device at a time.</li>
-                  <li class="list-group-item">Single user profile.</li>
-                  <li class="list-group-item">Basic playlist creation.</li>
-                </ul>
-              </div>
-
-              <div class="card-footer bg-white py-3">
-                <button type="button" class="btn btn-info btn-sm">Get it</button>
-              </div>
-
-            </div>
-            <!-- Card -->
-
-          </div>
-          <!--Grid column-->
-
-          <!--Grid column-->
-          <div class="col-lg-3 col-md-6 mb-4">
-
-            <!-- Card -->
-            <div class="card border border-primary">
-
-              <div class="card-header bg-white py-3">
-                <p class="text-uppercase small mb-2"><strong>Essential</strong></p>
-                <h5 class="mb-0">$19/month</h5>
-              </div>
-
-              <div class="card-body">
-                <ul  class="list-group list-group-flush">
-                  <li class="list-group-item">Expanded library access.</li>
-                  <li class="list-group-item">High Definition (HD) streaming.</li>
-                  <li class="list-group-item">Ad-free experience.</li>
-                  <li class="list-group-item">Limited offline downloads.</li>
-                  <li class="list-group-item">Limited to two devices at a time.</li>
-                  <li class="list-group-item">Multiple user profiles.</li>
-                  <li class="list-group-item"> Advanced playlist creation.</li>
-                  <li class="list-group-item"> Personalized content recommendations.</li>
-             
-                </ul>
-              </div>
-
-              <div class="card-footer bg-white py-3">
-                <button type="button" class="btn btn-info btn-sm">Buy now</button>
-              </div>
-
-            </div>
-            <!-- Card -->
-
-          </div>
-          <!--Grid column-->
-
-          <!--Grid column-->
-          <div class="col-lg-3 col-md-6 mb-4">
-
-            <!-- Card -->
-            <div class="card">
-
-              <div class="card-header bg-white py-3">
-                <p class="text-uppercase small mb-2"><strong>Advanced</strong></p>
-                <h5 class="mb-0">$49/month</h5>
-              </div>
-
-              <div class="card-body">
-                <ul  class="list-group list-group-flush">
-                  <li class="list-group-item">Premium library access, including exclusive content.</li>
-                  <li class="list-group-item">4K Ultra High Definition streaming.</li>
-                  <li class="list-group-item">Completely ad-free experience.</li>
-                  <li class="list-group-item">Unlimited offline downloads.</li>
-                  <li class="list-group-item">Simultaneous access on multiple devices.</li>
-                  <li class="list-group-item">Advanced user profile management.</li>
-                  <li class="list-group-item">Advanced playlist creation and sharing.</li>
-                  <li class="list-group-item">Enhanced personalized recommendations.</li>
-                  <li class="list-group-item">Early access to exclusive films and series.</li>
-                  <li class="list-group-item">Premium subscribers get early access.</li>
-                  <li class="list-group-item">Access to exclusive forums and discussions.</li>
-                  <li class="list-group-item">Priority customer support.</li>
-                  <li class="list-group-item">Extended free trial period.</li>
-                  <li class="list-group-item">Exclusive discounts on merchandise or additional services.</li>
-                  <li class="list-group-item">Additional payment options and flexibility.</li>
-                  
-                  
-                </ul>
-              </div>
-
-              <div class="card-footer bg-white py-3">
-                <button type="button" class="btn btn-info btn-sm">Buy now</button>
-              </div>
-
-            </div>
-            <!-- Card -->
-
-          </div>
-          <!--Grid column-->
-
-          <!--Grid column-->
-          <div class="col-lg-3 col-md-6 mb-4">
-
-            <!-- Card -->
-            <div class="card">
-
-              <div class="card-header bg-white py-3">
-                <p class="text-uppercase small mb-2"><strong>Enterprise</strong></p>
-                <h5 class="mb-0">$189/5 month</h5>
-              </div>
-
-              <div class="card-body">
-                <ul  class="list-group list-group-flush">
-                  <li class="list-group-item">Full access to the premium library, including exclusive and enterprise-specific content.</li>
-                  <li class="list-group-item">4K Ultra High Definition streaming for a cinematic experience.</li>
-                  <li class="list-group-item">Completely ad-free viewing for uninterrupted entertainment.</li>
-                  <li class="list-group-item">Unlimited offline downloads for on-the-go access.</li>
-                  <li class="list-group-item">Simultaneous access on multiple devices for team collaboration.</li>
-                  <li class="list-group-item">Advanced user profile management with customizable preferences.</li>
-                  <li class="list-group-item">Create, share, and collaborate on advanced playlists tailored to enterprise needs.</li>
-                  <li class="list-group-item">Highly personalized recommendations tailored for enterprise users.</li>
-                  <li class="list-group-item">Early access to exclusive enterprise-focused films, series, and behind-the-scenes content.</li>
-                  <li class="list-group-item">Enterprise subscribers get priority access to new releases.</li>
-                  <li class="list-group-item">Exclusive access to enterprise forums and discussions for networking and collaboration.</li>
-                  <li class="list-group-item">Priority 24/7 customer support for prompt issue resolution.</li>
-                  <li class="list-group-item">Extended free trial period to explore enterprise features.</li>
-                  <li class="list-group-item">Exclusive discounts on bulk subscriptions or additional enterprise services.</li>
-                  <li class="list-group-item">Flexible payment options, including invoicing for corporate convenience.</li>
-                  <li class="list-group-item">Enterprise-friendly cancellation policy with options for contract adjustments.</li>
-                  <li class="list-group-item">Enhanced security features and privacy settings with enterprise-grade protection.
-                  </li>
-
-                </ul>
-              </div>
-
-              <div class="card-footer bg-white py-3">
-                <button type="button" class="btn btn-info btn-sm">Buy now</button>
-              </div>
-
-            </div>
-            <!-- Card -->
-
-          </div>
-          <!--Grid column-->
-
-        </div>
-        <img width="100px" src="{{asset('image/series/logo.png')}}" alt="undermovies logo">
-      </section>
-      <!--Section: Content-->
-    </div>
-  </main>
-
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-
-  
 </body>
 </html>
