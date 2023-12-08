c11868b164c2bb127dc14347c73c329bb4d519d8
imadnajam <imad00naj11@gmail.com>
2023-12-08 23:26:10 +0100

    Update the pricing page

--- resources/views/pricing-template.blade.php
+++ resources/views/pricing-template.blade.php
@@ -1,82 +1,85 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Pricing Table</title>
-	<link href="https://fonts.googleapis.com/css?family=Maven+Pro:400,500,700&display=swap" rel="stylesheet">
-	<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-  <link rel="stylesheet" href="{{ asset('css/pricing1.css') }}">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Pricing Table</title>
+    <link href="https://fonts.googleapis.com/css?family=Maven+Pro:400,500,700&display=swap" rel="stylesheet">
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"
+        integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('css/pricing1.css') }}">
 </head>
+
 <body>
 
 
-<section class="table">
-	<div id="plan" class="table-header">
-		<span>misocial</span>
-		<h2>Select a Plan</h2>
-		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,<br> sed do eiusmod
-		tempor incididunt ut</p>
-	</div>
+    <section class="table">
+        <div id="plan" class="table-header">
+            <span>misocial</span>
+            <h2>Select a Plan</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,<br> sed do eiusmod
+                tempor incididunt ut</p>
+        </div>
 
-<div class="price-tags">
-	<div class="price-tag">
-	<img src="{{ asset('image/pricing/Asset3@4x.svg') }}" width="108" height="94">
-	 <span>personal</span>
-		<h4><sup>$</sup>0<sub>/month</sub></h4>
-		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-		tempor incididunt ut labore et dolore magna aliqua.</p>
-		<a href="#" class="price-btn">Select plan</a>
-	</div>
-     
-     <div class="price-tag">
-		<img src="{{ asset('image/pricing/Asset2@4x.svg') }}" width="120" height="108">
-		<span>personal</span>
-		<h4><sup>$</sup>29<sub>/month</sub></h4>
-		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-		tempor incididunt ut labore et dolore magna aliqua.</p>
-		<a href="#" class="price-btn">Select plan</a>
-	</div>
-     <div class="price-tag">
-		<img src="{{ asset('image/pricing/Asset1@4x.svg') }}" width="112" height="110">
-    
-		<span>personal</span>
-		<h4><sup>$</sup>49<sub>/month</sub></h4>
-		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-		tempor incididunt ut labore et dolore magna aliqua.</p>
-		<a href="#" class="price-btn">Select plan</a>
-	</div>
-</div>
-</section>
+        <div class="price-tags">
+            <div class="price-tag">
+                <img src="{{ asset('image/pricing/Asset3@4x.svg') }}" width="108" height="94">
+                <span>personal</span>
+                <h4><sup>$</sup>0<sub>/month</sub></h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua.</p>
+                <a href="#" class="price-btn">Select plan</a>
+            </div>
 
-<div class="cta">
-	<span>social marketing & analytics</span>
-	<h4>Take control of your data</h4>
-	<a href="#" class="cta-btn">Get started</a>
-	<a href="#" class="cta-btn">Explore features</a>
-</div>
+            <div class="price-tag">
+                <img src="{{ asset('image/pricing/Asset2@4x.svg') }}" width="120" height="108">
+                <span>personal</span>
+                <h4><sup>$</sup>29<sub>/month</sub></h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua.</p>
+                <a href="#" class="price-btn">Select plan</a>
+            </div>
+            <div class="price-tag">
+                <img src="{{ asset('image/pricing/Asset1@4x.svg') }}" width="112" height="110">
 
-<section class="footer-wrap">
-	<div class="footer">
-		<div class="logo">
-				<img src="{{ asset('image/series/logo.png') }}" width="100px">
-        
-	        </div>
+                <span>personal</span>
+                <h4><sup>$</sup>49<sub>/month</sub></h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua.</p>
+                <a href="#" class="price-btn">Select plan</a>
+            </div>
+        </div>
+    </section>
 
-<div class="line"></div>
-<div class="legal">
-<div class="copyright">
-	<span>© 2023  Undermovies & NJ. Make the right impression.</span>
-</div>
-<div class="social-btns">
-	<a href="#"><i class="fa fa-facebook"></i></a>
-	<a href="#"><i class="fa fa-twitter"></i></a>
-	<a href="#"><i class="fa fa-google-plus"></i></a>
-	<a href="#"><i class="fa fa-instagram"></i></a>
-</div>
-</div>
+    <div class="cta">
+        <span>social marketing & analytics</span>
+        <h4>Take control of your data</h4>
+        <a href="#" class="cta-btn">Get started</a>
+        <a href="#" class="cta-btn">Explore features</a>
+    </div>
 
-</section>
+    <section class="footer-wrap">
+        <div class="footer">
+            <div class="logo">
+                <img src="{{ asset('image/series/logo.png') }}" width="100px">
+
+            </div>
+
+            <div class="line"></div>
+            <div class="legal">
+                <div class="copyright">
+                    <span>© 2023 Undermovies & NJ. Make the right impression.</span>
+                </div>
+                <div class="social-btns">
+                    <a href="#"><i class="fa fa-facebook"></i></a>
+                    <a href="#"><i class="fa fa-twitter"></i></a>
+                    <a href="#"><i class="fa fa-google-plus"></i></a>
+                    <a href="#"><i class="fa fa-instagram"></i></a>
+                </div>
+            </div>
+
+    </section>
 
 
 
@@ -85,4 +88,5 @@
 
 
 </body>
+
 </html>
