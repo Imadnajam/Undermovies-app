274c816ee2ebc4a79adaeb6d8e119ae022d5e930
imadnajam <118115991+Imadnajam@users.noreply.github.com>
2024-01-01 17:04:25 +0100

    add the series detaile

--- app/Http/Controllers/seriesDetaileController.php
+++ app/Http/Controllers/seriesDetaileController.php
@@ -3,10 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+  use Illuminate\Support\Facades\Http;
 
 class seriesDetaileController extends Controller
 {
-    public function detail($id){
-  return view('components.seriesDetaile');
-    }
+
+  public function detail($id) {
+      // Make API request to fetch TV series details
+      $response = Http::get("https://api.themoviedb.org/3/tv/$id", [
+          'api_key' => '0269e1f69afd6ff169f8a6a2d9f0dc4d',
+      ]);
+  
+      // Check if the request was successful (status code 200)
+      if ($response->successful()) {
+          // Get the JSON-decoded data from the response
+          $tvSeriesData = $response->json();
+  
+          // Pass the data to the view
+          return view('components.seriesDetaile', ['tvSeriesData' => $tvSeriesData]);
+      } else {
+          // Handle the error, for example, redirect to an error page
+          return redirect()->route('error');
+      }
+  }
+  
 }
--- resources/views/components/seriesDetaile.blade.php
+++ resources/views/components/seriesDetaile.blade.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Free Guy 2021</title>
+  <title>{{$tvSeriesData['name']}}</title>
 
   <!-- 
     - favicon
@@ -164,7 +164,7 @@
 
           <figure class="movie-detail-banner">
 
-            <img src="./assets/images/movie-4.png" alt="Free guy movie poster">
+            <img src="{{'https://image.tmdb.org/t/p/w300_and_h450_bestv2/'.$tvSeriesData['backdrop_path']}}" alt="Free guy movie poster">
 
             <button class="play-btn">
               <ion-icon name="play-circle-outline"></ion-icon>
@@ -177,7 +177,7 @@
             <p class="detail-subtitle">New Episodes</p>
 
             <h1 class="h1 detail-title">
-              Free <strong>Guy</strong>
+              {{$tvSeriesData['name']}}
             </h1>
 
             <div class="meta-wrapper">
@@ -203,13 +203,13 @@
                 <div>
                   <ion-icon name="calendar-outline"></ion-icon>
 
-                  <time datetime="2021">2021</time>
+                  <time datetime="{{$tvSeriesData['first_air_date']}}">{{$tvSeriesData['first_air_date']}}</time>
                 </div>
 
                 <div>
                   <ion-icon name="time-outline"></ion-icon>
 
-                  <time datetime="PT115M">115 min</time>
+                  <time datetime="{{$tvSeriesData['popularity']}}">{{$tvSeriesData['popularity']}}</time>
                 </div>
 
               </div>
@@ -217,9 +217,10 @@
             </div>
 
             <p class="storyline">
-              A bank teller called Guy realizes he is a background character in an open world video game called Free
-              City that will
-              soon go offline.
+              {{$tvSeriesData['overview']}}
+              
+              
+              
             </p>
 
             <div class="details-actions">
@@ -266,31 +267,30 @@
       <section class="tv-series">
         <div class="container">
 
-          <p class="section-subtitle">Best TV Series</p>
+          <p class="section-subtitle">seasons</p>
 
-          <h2 class="h2 section-title">World Best TV Series</h2>
 
           <ul class="movies-list">
-
+            @foreach ($tvSeriesData['seasons'] as $season)
             <li>
               <div class="movie-card">
 
-                <a href="./movie-details.html">
+                <a href="#">
                   <figure class="card-banner">
-                    <img src="./assets/images/series-1.png" alt="Moon Knight movie poster">
+                    <img src="https://image.tmdb.org/t/p/w500{{ $season['poster_path'] }}" alt="{{ $season['name'] }}" alt="{{ $season['name'] }}">
                   </figure>
                 </a>
 
                 <div class="title-wrapper">
-                  <a href="./movie-details.html">
-                    <h3 class="card-title">Moon Knight</h3>
+                  <a href="#">
+                    <h3 class="card-title">{{ $season['name'] }}</h3>
                   </a>
 
-                  <time datetime="2022">2022</time>
+                  <time datetime="{{ $season['air_date'] }}">{{ $season['air_date'] }}</time>
                 </div>
 
                 <div class="card-meta">
-                  <div class="badge badge-outline">2K</div>
+                  <div class="badge badge-outline">Episode Count: {{ $season['episode_count'] }}</div>
 
                   <div class="duration">
                     <ion-icon name="time-outline"></ion-icon>
@@ -301,121 +301,15 @@
                   <div class="rating">
                     <ion-icon name="star"></ion-icon>
 
-                    <data>8.6</data>
+                    <data>{{ $season['vote_average'] }}</data>
                   </div>
                 </div>
 
               </div>
             </li>
 
-            <li>
-              <div class="movie-card">
-
-                <a href="./movie-details.html">
-                  <figure class="card-banner">
-                    <img src="./assets/images/series-2.png" alt="Halo movie poster">
-                  </figure>
-                </a>
-
-                <div class="title-wrapper">
-                  <a href="./movie-details.html">
-                    <h3 class="card-title">Halo</h3>
-                  </a>
-
-                  <time datetime="2022">2022</time>
-                </div>
-
-                <div class="card-meta">
-                  <div class="badge badge-outline">2K</div>
-
-                  <div class="duration">
-                    <ion-icon name="time-outline"></ion-icon>
-
-                    <time datetime="PT59M">59 min</time>
-                  </div>
-
-                  <div class="rating">
-                    <ion-icon name="star"></ion-icon>
-
-                    <data>8.8</data>
-                  </div>
-                </div>
-
-              </div>
-            </li>
-
-            <li>
-              <div class="movie-card">
-
-                <a href="./movie-details.html">
-                  <figure class="card-banner">
-                    <img src="./assets/images/series-3.png" alt="Vikings: Valhalla movie poster">
-                  </figure>
-                </a>
-
-                <div class="title-wrapper">
-                  <a href="./movie-details.html">
-                    <h3 class="card-title">Vikings: Valhalla</h3>
-                  </a>
-
-                  <time datetime="2022">2022</time>
-                </div>
-
-                <div class="card-meta">
-                  <div class="badge badge-outline">2K</div>
-
-                  <div class="duration">
-                    <ion-icon name="time-outline"></ion-icon>
-
-                    <time datetime="PT51M">51 min</time>
-                  </div>
-
-                  <div class="rating">
-                    <ion-icon name="star"></ion-icon>
-
-                    <data>8.3</data>
-                  </div>
-                </div>
-
-              </div>
-            </li>
-
-            <li>
-              <div class="movie-card">
-
-                <a href="./movie-details.html">
-                  <figure class="card-banner">
-                    <img src="./assets/images/series-4.png" alt="Money Heist movie poster">
-                  </figure>
-                </a>
-
-                <div class="title-wrapper">
-                  <a href="./movie-details.html">
-                    <h3 class="card-title">Money Heist</h3>
-                  </a>
-
-                  <time datetime="2017">2017</time>
-                </div>
-
-                <div class="card-meta">
-                  <div class="badge badge-outline">4K</div>
-
-                  <div class="duration">
-                    <ion-icon name="time-outline"></ion-icon>
-
-                    <time datetime="PT70M">70 min</time>
-                  </div>
-
-                  <div class="rating">
-                    <ion-icon name="star"></ion-icon>
-
-                    <data>8.3</data>
-                  </div>
-                </div>
-
-              </div>
-            </li>
-
+           
+            @endforeach
           </ul>
 
         </div>
@@ -533,7 +427,7 @@
           &copy; 2023 <a href="#">NJ</a>. All Rights Reserved
         </p>
 
-        <img src="{{ asset('image/series/logo.png') }}" alt="Online banking companies logo" class="footer-bottom-img">
+        <img style="width: 100px" src="{{ asset('image/series/logo.png') }}" alt="Online banking companies logo" class="footer-bottom-img">
 
       </div>
     </div>
