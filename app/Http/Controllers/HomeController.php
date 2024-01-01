da7161fc4f21584d821c1fbe96bb1bc6a7df5575
imadnajam <118115991+Imadnajam@users.noreply.github.com>
2024-01-01 17:41:04 +0100

    add the series detaile

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Http;
 class HomeController extends Controller
 {
     public function index()
@@ -14,16 +14,30 @@ class HomeController extends Controller
          $films = json_decode($jsonContent, true);
 
          /////////////////////////////////////
-         $filePath = storage_path('app/home2.json');
-         $jsonContent = file_get_contents($filePath);
-         $tvs = json_decode($jsonContent, true);
+
+         $response = Http::get("https://api.themoviedb.org/3/tv/top_rated", [
+            'api_key' => '0269e1f69afd6ff169f8a6a2d9f0dc4d',
+        ]);
+        $filePath = storage_path('app/home1.json');
+        $jsonContent = file_get_contents($filePath);
+        $tops = json_decode($jsonContent, true);
+        // Check if the request was successful (status code 200)
+        if ($response->successful()) {
+            // Get the JSON-decoded data from the response
+            $tvs = $response->json();
+    
+            // Pass the data to the view
+            return view('index',['films'=>$films,'tvs'=>$tvs,'tops'=>$tops]);
+        } else {
+            // Handle the error, for example, redirect to an error page
+            return redirect()->route('error');
+        }
+        
          ///////////////////////////////////////
-         $filePath = storage_path('app/home1.json');
-         $jsonContent = file_get_contents($filePath);
-         $tops = json_decode($jsonContent, true);
+        
 
          // Return the JSON data
-         return view('index',['films'=>$films,'tvs'=>$tvs,'tops'=>$tops]);
+        
        
     }
 }
--- app/Http/Controllers/seriesDetaileController.php
+++ app/Http/Controllers/seriesDetaileController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-  use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Http;
 
 class seriesDetaileController extends Controller
 {
