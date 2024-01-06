9acd25ecf3d892185342849eba27e2ce2e2e2507
imadnajam <118115991+Imadnajam@users.noreply.github.com>
2024-01-06 12:52:00 +0100

    display episodes trailler

--- resources/views/films.blade.php
+++ resources/views/films.blade.php
@@ -17,9 +17,7 @@
     <link rel="stylesheet" href="{{ asset('css/home.css') }}">
 
 
-    <!--
-    - google font link
-  -->
+
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
