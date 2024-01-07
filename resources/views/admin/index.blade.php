636c82a94f703c104bfae5726ea7fce6077de580
imadnajam <118115991+Imadnajam@users.noreply.github.com>
2024-01-07 12:44:38 +0100

    update admin page

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -214,7 +214,7 @@
                     </div>
                 </div>
                 <footer class="pt-5 d-flex justify-content-between">
-                    <span>Copyright © 2019-2020 <a href="https://themesberg.com">Themesberg</a></span>
+                  
                     <ul class="nav m-0">
                         <li class="nav-item">
                           <a class="nav-link text-secondary" aria-current="page" href="#">Privacy Policy</a>
