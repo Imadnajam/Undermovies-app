9d81e9b3f9dee19b790e56f8425ce8268f8d3fbe
imadnajam <118115991+Imadnajam@users.noreply.github.com>
2024-01-12 20:09:33 +0100

    work with include methode

---
+++ resources/views/frontend/components/headlinks.blade.php
@@ -0,0 +1,9 @@
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>UnderMovie - Home</title>
+<link rel="shortcut icon" href="./favicon.svg" type="image/svg+xml">
+<link rel="stylesheet" href="{{ asset('css/home.css') }}">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,15 +2,7 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>UnderMovie - Home</title>
-    <link rel="shortcut icon" href="./favicon.svg" type="image/svg+xml">
-    <link rel="stylesheet" href="{{ asset('css/home.css') }}">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
+    @include('frontend.components.headlinks')
 </head>
 
 <body id="top">
@@ -45,12 +37,15 @@
 
         </article>
     </main>
+
     <footer class="footer">
         @include('frontend.components.footer')
     </footer>
     <a href="#top" class="go-top" data-go-top>
         <ion-icon name="chevron-up"></ion-icon>
     </a>
+
+
     <script src="{{ asset('js/home.js') }}"></script>
     <script>
         var backgroundList = @json($list);
