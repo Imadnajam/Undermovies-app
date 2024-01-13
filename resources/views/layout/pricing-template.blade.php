26723c920676eac44087df8afa88ce3d22c71714
imadnajam <118115991+Imadnajam@users.noreply.github.com>
2024-01-13 20:36:44 +0100

    update pricing page

--- resources/views/layout/pricing-template.blade.php
+++ resources/views/layout/pricing-template.blade.php
@@ -2,13 +2,9 @@
 <html lang="en">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Pricing Table</title>
-    <link href="https://fonts.googleapis.com/css?family=Maven+Pro:400,500,700&display=swap" rel="stylesheet">
-    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"
-        integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{ asset('css/pricing1.css') }}">
+    
+    @include('frontend.components.pricingHead')
+    
 </head>
 
 <body>
