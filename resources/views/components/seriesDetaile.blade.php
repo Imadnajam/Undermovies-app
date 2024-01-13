fbf14ab578c5cf9968e9532a04aec9ef5c62581d
imadnajam <118115991+Imadnajam@users.noreply.github.com>
2024-01-13 19:30:44 +0100

    add share script component

--- resources/views/components/seriesDetaile.blade.php
+++ resources/views/components/seriesDetaile.blade.php
@@ -467,35 +467,7 @@
 
   <script src="{{ asset('js/home.js') }}"></script>
   
-  <script>
-   document.addEventListener('DOMContentLoaded', function() {
-    // Get the WhatsApp and Facebook share buttons
-    const whatsappShareButton = document.getElementById('whatsappShare');
-    const facebookShareButton = document.getElementById('facebookShare');
-
-    // Add a click event listener to the WhatsApp share button
-    whatsappShareButton.addEventListener('click', function() {
-        // Generate the sharing message based on your movie details
-        const shareMessage = `${$movie['original_title']} - ${$movie['overview']}\n${window.location.href}`;
-
-        // Open the WhatsApp sharing link with the message
-        const whatsappLink = `https://wa.me/?text=${encodeURIComponent(shareMessage)}`;
-        window.open(whatsappLink, '_blank');
-    });
-
-    // Add a click event listener to the Facebook share button
-    facebookShareButton.addEventListener('click', function() {
-        // Generate the sharing link based on your movie details
-        const shareLink = window.location.href; // You can customize this based on your requirements
-
-        // Open the Facebook sharing link
-        const facebookLink =
-            `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(shareLink)}`;
-        window.open(facebookLink, '_blank');
-    });
-});
-
-</script>
+  @include('frontend.components.sharescript')
 
   <!-- 
     - ionicon link
