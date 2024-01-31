46bd60c959c12d62afbce47d743bc926b7431601
imadnajam <118115991+Imadnajam@users.noreply.github.com>
2024-01-31 12:06:16 +0100

    add the transction langue in pricing page French

--- resources/lang/fr/pricing.php
+++ resources/lang/fr/pricing.php
@@ -1,19 +1,20 @@
 <?php
 return [
-    'JTHP' => 'Unisciti al Piano Hobby',
-    'SP' => 'Seleziona un Piano',
-    'SPP' => 'Migliora la tua esperienza di visione con i nostri piani premium.',
-    'Basic' => 'Base',
-    'month' => 'mese',
-    'BasicDec' => 'Accedi a una selezione limitata di film in definizione standard (SD).',
-    'Selectplan' => 'Seleziona piano',
+    'JTHP' => 'Rejoignez le Plan Hobby',
+    'SP' => 'Sélectionnez un Plan',
+    'SPP' => 'Améliorez votre expérience de visionnage avec nos plans premium.',
+    'Basic' => 'Basique',
+    'month' => 'mois',
+    'BasicDec' => 'Accédez à une sélection limitée de films en définition standard (SD).',
+    'Selectplan' => 'Sélectionnez un plan',
     'Standard' => 'Standard',
-    'StandardDec' => 'Goditi lo streaming in alta definizione (HD) e una libreria più ampia di film.',
+    'StandardDec' => 'Profitez du streaming haute définition (HD) et d une bibliothèque de films élargie.',
     'Premium' => 'Premium',
-    'PremiumDec' => 'Sblocca lo streaming in ultra alta definizione (UHD) e contenuti esclusivi.',
-    'Cta1' => 'Accesso esclusivo alle nuove uscite',
-    'Cta2' => 'Prendi il controllo delle tue serate cinematografiche',
-    'Cta3' => 'Inizia',
-    'Cta4' => 'Esplora le caratteristiche',
-    'copyright' => '© 2023 Undermovies NJ. Tutti i diritti riservati.'
+    'PremiumDec' => 'Débloquez le streaming ultra haute définition (UHD) et le contenu exclusif.',
+    'Cta1' => 'Accès exclusif aux nouvelles sorties',
+    'Cta2' => 'Prenez le contrôle de vos soirées cinéma',
+    'Cta3' => 'Commencez',
+    'Cta4' => 'Explorez les fonctionnalités',
+    'copyright' => '© 2023 Undermovies NJ. Tous droits réservés.'
 ];
+
---
+++ resources/lang/it/pricing.php
@@ -0,0 +1,19 @@
+<?php
+return [
+    'JTHP' => 'Unisciti al Piano Hobby',
+    'SP' => 'Seleziona un Piano',
+    'SPP' => 'Migliora la tua esperienza di visione con i nostri piani premium.',
+    'Basic' => 'Base',
+    'month' => 'mese',
+    'BasicDec' => 'Accedi a una selezione limitata di film in definizione standard (SD).',
+    'Selectplan' => 'Seleziona piano',
+    'Standard' => 'Standard',
+    'StandardDec' => 'Goditi lo streaming in alta definizione (HD) e una libreria più ampia di film.',
+    'Premium' => 'Premium',
+    'PremiumDec' => 'Sblocca lo streaming in ultra alta definizione (UHD) e contenuti esclusivi.',
+    'Cta1' => 'Accesso esclusivo alle nuove uscite',
+    'Cta2' => 'Prendi il controllo delle tue serate cinematografiche',
+    'Cta3' => 'Inizia',
+    'Cta4' => 'Esplora le caratteristiche',
+    'copyright' => '© 2023 Undermovies NJ. Tutti i diritti riservati.'
+];
