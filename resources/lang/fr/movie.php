5432069cb8b5b2c50aa21f0a43a2d5558b921a18
imadnajam <118115991+Imadnajam@users.noreply.github.com>
2024-01-31 20:23:49 +0100

    add the transction langue in Movie page French

---
+++ resources/lang/fr/movie.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'OnlineStreaming' => 'Diffusion en ligne',
+    'TRM' => 'Meilleurs films notés',
+    'Aventure' => 'Aventure',
+    'Histoire' => 'Histoire',
+    'Action' => 'Action',
+    'Policier' => 'Policier',
+    'Science fiction' => 'Science-fiction',
+    'fantastique' => 'Fantastique',
+    'horreur' => 'Horreur',
+];
