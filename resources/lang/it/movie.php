e0a554929ba96895e09bb57593162b67de7948ad
imadnajam <118115991+Imadnajam@users.noreply.github.com>
2024-01-31 20:24:48 +0100

    add the transction langue in Movie page Italian

---
+++ resources/lang/it/movie.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'OnlineStreaming' => 'Streaming online',
+    'TRM' => 'Film più votati',
+    'Aventure' => 'Avventura',
+    'Histoire' => 'Storia',
+    'Action' => 'Azione',
+    'Policier' => 'Poliziesco',
+    'Science fiction' => 'Fantascienza',
+    'fantastique' => 'Fantastico',
+    'horreur' => 'Horror',
+];
