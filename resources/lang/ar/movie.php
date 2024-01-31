dc0cbc36eacbee0a820653d92748fcb44b4c021e
imadnajam <118115991+Imadnajam@users.noreply.github.com>
2024-01-31 20:22:45 +0100

    add the transction langue in Movie page Arabe

---
+++ resources/lang/ar/movie.php
@@ -0,0 +1,14 @@
+<?php
+
+
+return [
+    'OnlineStreaming' => 'بث مباشر عبر الإنترنت',
+    'TRM' => 'أعلى تصنيف الأفلام',
+    'Aventure' => 'مغامرة',
+    'Histoire' => 'تاريخ',
+    'Action' => 'حركة',
+    'Policier' => 'شرطة/جريمة',
+    'Science fiction' => 'خيال علمي',
+    'fantastique' => 'خيالي',
+    'horreur' => 'رعب',
+];
