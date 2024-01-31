a4bccfcd36bd373226d1210f4d3c2168a45eaa9d
imadnajam <118115991+Imadnajam@users.noreply.github.com>
2024-01-31 12:03:51 +0100

    add the transction langue in pricing page German

--- resources/lang/al/pricing.php
+++ resources/lang/al/pricing.php
@@ -1,5 +1,21 @@
 <?php
 
 return [
- 
+    'JTHP' => 'Treten Sie dem Hobby Plan bei',
+    'SP' => 'Wählen Sie einen Plan aus',
+    'SPP' => 'Verbessern Sie Ihr Filmerlebnis mit unseren Premium-Plänen.',
+    'Basic' => 'Grundlegend',
+    'month' => 'Monat',
+    'BasicDec' => 'Zugriff auf eine begrenzte Auswahl von Filmen in Standarddefinition (SD).',
+    'Selectplan' => 'Plan auswählen',
+    'Standard' => 'Standard',
+    'StandardDec' => 'Genießen Sie hochauflösendes (HD) Streaming und eine umfangreichere Filmbibliothek.',
+    'Premium' => 'Premium',
+    'PremiumDec' => 'Entsperren Sie Ultra-High-Definition (UHD) Streaming und exklusive Inhalte.',
+    'Cta1' => 'Exklusiver Zugang zu neuen Veröffentlichungen',
+    'Cta2' => 'Behalten Sie die Kontrolle über Ihre Filmabende',
+    'Cta3' => 'Legen Sie los',
+    'Cta4' => 'Erkunden Sie die Funktionen',
+    'copyright' => '© 2023 Undermovies NJ. Alle Rechte vorbehalten.'
 ];
+
