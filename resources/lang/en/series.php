d0ba99f330af3e0f68e447c30fc4ac891d788268
imadnajam <118115991+Imadnajam@users.noreply.github.com>
2024-02-02 17:23:23 +0100

    lang transalte in series page English

---
+++ resources/lang/en/series.php
@@ -0,0 +1,78 @@
+<?php
+
+
+return[
+  'AA'=>'Action/Adventure',
+  'explore'=>'explore',
+  'Comedy'=>'Comedy',
+  'SFF'=>'Science Fiction/Fantasy',
+  'Horror'=>'Horror',
+  'CM'=>'Crime/Mystery',
+  'text1'=>'series are known for intense and dynamic action sequences. These can include
+                fights, chases, and daring feats that keep the audience on the edge of their seats.
+                The storyline often involves characters embarking on adventurous journeys, quests, or missions.
+                These adventures can take place in exotic or dangerous locations, adding an element of exploration
+                and discovery to the narrative.
+                The main characters typically possess special skills or abilities that contribute to their success
+                in overcoming challenges. These skills may range from combat proficiency and survival instincts to
+                intelligence and technological expertise.
+                There is usually a central conflict or antagonist that the protagonists must face. This could be a
+                villain, an evil organization, or a force threatening the well-being of the characters or the world
+                they inhabit.
+                These series may take place in a variety of settings, ranging from real-world locations to fantasy
+                realms. The diverse settings contribute to the sense of adventure and exploration.
+                Episodes can be standalone adventures or contribute to an overarching storyline. A balance between
+                episodic and serialized elements keeps the audience engaged and encourages them to tune in
+                regularly.',
+  'text2'=>'Humorous Premise: Comedy series typically have a humorous concept or premise that serves as the
+            foundation for the show. This could be centered around a workplace, a group of friends, a family, or
+            any other relatable setting.
+
+            Ensemble Cast: Comedy series often feature an ensemble cast of characters, each contributing to the
+            humor in their unique way. These characters may have distinct personalities and quirks that lead to
+            comedic interactions.
+
+            Situation-Based Humor: The humor in a comedy series often arises from specific situations or
+            scenarios. These situations can be relatable, absurd, or exaggerated, but they usually lead to
+            comedic outcomes.
+
+            Wordplay and Wit: Clever wordplay, puns, and witty dialogue are common elements in comedy series.
+            The writing is crafted to elicit laughter through clever language and comedic timing.
+
+            Recurring Themes and Running Gags: Many comedy series incorporate recurring themes or running gags
+            that become familiar to the audience. These elements contribute to the show s identity and can be a
+            source of ongoing humor.
+
+            Physical Comedy: Physical humor, including slapstick comedy and visual gags, is often used to
+            generate laughs. Characters may find themselves in awkward or absurd situations that lead to comedic
+            outcomes.
+
+            Breaking the Fourth Wall: Some comedy series involve characters breaking the fourth wall,
+            acknowledging the audience directly, or engaging in meta-humor. This can create a self-aware and
+            playful dynamic.
+
+            Character Development: While the primary goal is to make the audience laugh, successful comedy
+            series often incorporate character development. Characters may learn, grow, or face challenges in a
+            humorous way.
+
+            Episodic Structure: Many comedy series follow an episodic structure, where each episode presents a
+            self-contained story or situation. This allows viewers to enjoy episodes independently, although
+            there may be an overarching storyline.
+
+            Social Commentary: Some comedy series use humor to provide social commentary or satirize real-world
+            situations. This adds depth to the comedy and may resonate with a broader audience.',
+  'text3'=>'A science fiction/fantasy series is a television genre that combines elements of science fiction and
+           fantasy in its storytelling. These series often explore speculative and imaginative concepts,
+           futuristic settings, advanced technologies, and fantastical elements. Here are key features and
+           elements commonly found in science fiction/fantasy series',
+
+ 'text4'=>'A horror series is a television genre that aims to evoke fear, suspense, and a sense of dread in its
+          audience. These series often explore themes of the supernatural, psychological terror, and the
+          unknown. Here are key features and elements commonly found in horror series',
+ 'text5'=>'Crime/mystery is a genre of television programming that revolves around the investigation and
+            resolution of criminal cases. These series often combine elements of suspense, intrigue, and
+            detective work, drawing viewers into the process of solving crimes. Here are key features and
+            elements commonly found in crime/mystery series',
+
+
+];
--- resources/views/frontend/components/seriescontentP.blade.php
+++ resources/views/frontend/components/seriescontentP.blade.php
@@ -10,30 +10,30 @@
     <nav class="menu">
         <div class="menu__item">
             <span class="menu__item-number">01</span>
-            <span class="menu__item-textwrap"><span class="menu__item-text">Action/Adventure</span></span>
-            <a class="menu__item-link">explore</a>
+            <span class="menu__item-textwrap"><span class="menu__item-text">{{__('series.AA')}} </span></span>
+            <a class="menu__item-link">{{__('series.explore')}}</a>
         </div>
         <div class="menu__item">
             <span class="menu__item-number">02</span>
-            <span class="menu__item-textwrap"><span class="menu__item-text">Comedy</span></span>
-            <a class="menu__item-link">explore</a>
+            <span class="menu__item-textwrap"><span class="menu__item-text">{{__('series.Comedy')}}</span></span>
+            <a class="menu__item-link">{{__('series.explore')}}</a>
         </div>
         <div class="menu__item">
             <span class="menu__item-number">03</span>
-            <span class="menu__item-textwrap"><span class="menu__item-text">Science Fiction/Fantasy
+            <span class="menu__item-textwrap"><span class="menu__item-text">{{__('series.SFF')}}
                 </span></span>
-            <a class="menu__item-link">explore</a>
+            <a class="menu__item-link">{{__('series.explore')}}</a>
         </div>
         <div class="menu__item">
             <span class="menu__item-number">04</span>
-            <span class="menu__item-textwrap"><span class="menu__item-text">Horror</span></span>
-            <a class="menu__item-link">explore</a>
+            <span class="menu__item-textwrap"><span class="menu__item-text">{{__('series.Horror')}}</span></span>
+            <a class="menu__item-link">{{__('series.explore')}}</a>
         </div>
         <div class="menu__item">
             <span class="menu__item-number">05</span>
-            <span class="menu__item-textwrap"><span class="menu__item-text">Crime/Mystery
+            <span class="menu__item-textwrap"><span class="menu__item-text">{{__('series.CM')}}
                 </span></span>
-            <a class="menu__item-link">explore</a>
+            <a class="menu__item-link">{{__('series.explore')}}</a>
         </div>
     </nav>
     <div class="page page--preview">
@@ -97,78 +97,21 @@
         </div><!-- /gridwrap -->
         <div class="content">
             <div class="content__item">
-                Action/adventure series are known for intense and dynamic action sequences. These can include
-                fights, chases, and daring feats that keep the audience on the edge of their seats.
-                The storyline often involves characters embarking on adventurous journeys, quests, or missions.
-                These adventures can take place in exotic or dangerous locations, adding an element of exploration
-                and discovery to the narrative.
-                The main characters typically possess special skills or abilities that contribute to their success
-                in overcoming challenges. These skills may range from combat proficiency and survival instincts to
-                intelligence and technological expertise.
-                There is usually a central conflict or antagonist that the protagonists must face. This could be a
-                villain, an evil organization, or a force threatening the well-being of the characters or the world
-                they inhabit.
-                These series may take place in a variety of settings, ranging from real-world locations to fantasy
-                realms. The diverse settings contribute to the sense of adventure and exploration.
-                Episodes can be standalone adventures or contribute to an overarching storyline. A balance between
-                episodic and serialized elements keeps the audience engaged and encourages them to tune in
-                regularly.
+                {{__('series.text1')}}
+                
             </div>
             <div class="content__item">
-                Humorous Premise: Comedy series typically have a humorous concept or premise that serves as the
-                foundation for the show. This could be centered around a workplace, a group of friends, a family, or
-                any other relatable setting.
-
-                Ensemble Cast: Comedy series often feature an ensemble cast of characters, each contributing to the
-                humor in their unique way. These characters may have distinct personalities and quirks that lead to
-                comedic interactions.
-
-                Situation-Based Humor: The humor in a comedy series often arises from specific situations or
-                scenarios. These situations can be relatable, absurd, or exaggerated, but they usually lead to
-                comedic outcomes.
-
-                Wordplay and Wit: Clever wordplay, puns, and witty dialogue are common elements in comedy series.
-                The writing is crafted to elicit laughter through clever language and comedic timing.
-
-                Recurring Themes and Running Gags: Many comedy series incorporate recurring themes or running gags
-                that become familiar to the audience. These elements contribute to the show's identity and can be a
-                source of ongoing humor.
-
-                Physical Comedy: Physical humor, including slapstick comedy and visual gags, is often used to
-                generate laughs. Characters may find themselves in awkward or absurd situations that lead to comedic
-                outcomes.
-
-                Breaking the Fourth Wall: Some comedy series involve characters breaking the fourth wall,
-                acknowledging the audience directly, or engaging in meta-humor. This can create a self-aware and
-                playful dynamic.
-
-                Character Development: While the primary goal is to make the audience laugh, successful comedy
-                series often incorporate character development. Characters may learn, grow, or face challenges in a
-                humorous way.
-
-                Episodic Structure: Many comedy series follow an episodic structure, where each episode presents a
-                self-contained story or situation. This allows viewers to enjoy episodes independently, although
-                there may be an overarching storyline.
-
-                Social Commentary: Some comedy series use humor to provide social commentary or satirize real-world
-                situations. This adds depth to the comedy and may resonate with a broader audience.
-            </div>
+                {{__('series.text2')}}
+                </div>
             <div class="content__item">
-                A science fiction/fantasy series is a television genre that combines elements of science fiction and
-                fantasy in its storytelling. These series often explore speculative and imaginative concepts,
-                futuristic settings, advanced technologies, and fantastical elements. Here are key features and
-                elements commonly found in science fiction/fantasy series
-            </div>
+                {{__('series.text3')}}
+           </div>
             <div class="content__item">
-                A horror series is a television genre that aims to evoke fear, suspense, and a sense of dread in its
-                audience. These series often explore themes of the supernatural, psychological terror, and the
-                unknown. Here are key features and elements commonly found in horror series
-            </div>
+                {{__('series.text4')}}
+                 </div>
             <div class="content__item">
-                Crime/mystery is a genre of television programming that revolves around the investigation and
-                resolution of criminal cases. These series often combine elements of suspense, intrigue, and
-                detective work, drawing viewers into the process of solving crimes. Here are key features and
-                elements commonly found in crime/mystery series
+                {{__('series.text5')}}
+             
             </div>
         </div>
     </div><!-- /page -->
