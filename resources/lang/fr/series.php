e3de2727faa8742731030ac484050d93970ce689
imadnajam <118115991+Imadnajam@users.noreply.github.com>
2024-02-02 17:43:47 +0100

    lang transalte in series page French

---
+++ resources/lang/fr/series.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'AA' => 'Action/Aventure',
+    'explore' => 'Explorer',
+    'Comedy' => 'Comédie',
+    'SFF' => 'Science-Fiction/Fantaisie',
+    'Horror' => 'Horreur',
+    'CM' => 'Crime/Mystère',
+    'text1' => 'Les séries passionnantes avec des événements excitants et captivants. Elles peuvent inclure des combats, des poursuites et des aventures qui maintiennent le public captivé. Les personnages ont souvent des compétences spéciales qui les aident à surmonter les défis dans des voyages épiques.',
+    'text2' => 'Les séries comiques reposent sur des idées humoristiques et des situations drôles. Cela inclut une équipe de personnages comiques et de l humour dérivé de situations et de dialogues amusants.',
+    'text3' => 'Les séries fantastiques combinent la science-fiction et la fantaisie, explorant des idées innovantes et des mondes imaginaires avancés. Il y a une focalisation sur les concepts futuristes et la technologie avancée.',
+    'text4' => 'Les séries d horreur visent à susciter la terreur et l excitation. Elles explorent le surnaturel, l horreur psychologique, et comportent des éléments de mystère et d inconnu.',
+    'text5' => 'Les séries criminelles et mystérieuses tournent autour de l enquête et de la résolution criminelle, combinant suspense et investigation pour attirer les téléspectateurs.',
+];
