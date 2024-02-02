bac00329eb2291c9513554161a7dc5628069bf8b
imadnajam <118115991+Imadnajam@users.noreply.github.com>
2024-02-02 11:01:49 +0100

    lang transalte in anime page Italy

---
+++ resources/lang/it/anime.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'OS' => 'Streaming online',
+    'TRA' => 'Anime più votati',
+    'SearchA' => 'Ricerca per nome dell\'anime',
+    'Search' => 'Ricerca',
+    'Shounen' => 'Shounen',
+    'Seinen' => 'Seinen',
+    'Shoujo' => 'Shoujo',
+    'Josei' => 'Josei',
+    'Isekai' => 'Isekai',
+    'Slice' => 'Slice of Life',
+    'fantastique' => 'Fantastico',
+    'Mecha' => 'Mecha',
+    'Psychological' => 'Psicologico',
+    'Romance' => 'Romantico',
+    'Horror' => 'Horror',
+    'Comedy' => 'Commedia',
+   ];
+   
