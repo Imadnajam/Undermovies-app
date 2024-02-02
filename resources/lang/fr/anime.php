95a2af8b9c018ae8ddb1b50edf5eaf1f8f4f844a
imadnajam <118115991+Imadnajam@users.noreply.github.com>
2024-02-02 11:00:41 +0100

    lang transalte in anime page French

---
+++ resources/lang/fr/anime.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'OS' => 'Streaming en ligne',
+    'TRA' => 'Anime les mieux notés',
+    'SearchA' => 'Rechercher par nom d\'anime',
+    'Search' => 'Rechercher',
+    'Shounen' => 'Shounen',
+    'Seinen' => 'Seinen',
+    'Shoujo' => 'Shoujo',
+    'Josei' => 'Josei',
+    'Isekai' => 'Isekai',
+    'Slice' => 'Tranche de vie',
+    'fantastique' => 'Fantastique',
+    'Mecha' => 'Mecha',
+    'Psychological' => 'Psychologique',
+    'Romance' => 'Romance',
+    'Horror' => 'Horreur',
+    'Comedy' => 'Comédie',
+   ];
+   
