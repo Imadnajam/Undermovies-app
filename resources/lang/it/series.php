47505297a6169179443fe15f3d1f6b02bad15277
imadnajam <118115991+Imadnajam@users.noreply.github.com>
2024-02-02 17:45:32 +0100

    lang transalte in series page Italy

---
+++ resources/lang/it/series.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'AA' => 'Azione/Avventura',
+    'explore' => 'Esplorazione',
+    'Comedy' => 'Commedia',
+    'SFF' => 'Fantascienza/Fantasia',
+    'Horror' => 'Horror',
+    'CM' => 'Crimine/Mistero',
+    'text1' => 'Serie avvincenti con eventi emozionanti e avvincenti. Possono includere combattimenti, inseguimenti e avventure che tengono il pubblico incollato. I personaggi spesso possiedono abilità speciali che li aiutano a superare sfide in epiche avventure.',
+    'text2' => 'Le serie comiche si basano su idee umoristiche e situazioni divertenti. Ciò include un gruppo di personaggi divertenti e umorismo derivato da situazioni e dialoghi divertenti.',
+    'text3' => 'Le serie di genere fantastico fondono fantascienza e fantasia, esplorando idee innovative e mondi immaginari avanzati. Vi è un focus su concetti futuristici e tecnologia avanzata.',
+    'text4' => 'Le serie horror mirano a suscitare terrore ed eccitazione. Esplorano il sovrannaturale, l orrore psicologico e contengono elementi di mistero e ignoto.',
+    'text5' => 'Le serie criminali e misteriose ruotano attorno all indagine e alla risoluzione criminale, unendo suspense e indagine per attirare gli spettatori.',
+];
