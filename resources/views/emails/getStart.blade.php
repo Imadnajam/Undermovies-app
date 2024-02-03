42c589712b5cd6a076bd51d6aec22183337bbb70
imadnajam <118115991+Imadnajam@users.noreply.github.com>
2024-02-03 18:24:53 +0100

    Get_started send email

---
+++ resources/views/emails/getStart.blade.php
@@ -0,0 +1,17 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to Undermovies</title>
+</head>
+<body>
+    <div style="text-align: center; padding: 20px;">
+        <img src="{{ $message->embed(public_path('image/home/logo.png')) }}" alt="Logo" style="max-width: 100px;">
+        <h1>About Our Movie & Series Streaming Web Application</h1>
+        <p>Welcome to our Movie & Series Streaming Web Application. This web application allows users to stream and enjoy a wide range of films and series. It is designed to be user-friendly, secure, and scalable.</p>
+        <p>Explore our vast collection of movies and series, and immerse yourself in a world of entertainment.</p>
+    </div>
+</body>
+</html>
